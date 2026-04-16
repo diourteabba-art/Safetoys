422d3f9c649ecadaa0704165e73e6b5198c1bbfc
diourteabba-art <diourteabba@gmail.com>
2026-04-16 23:51:43 +0200

    Update [id].js

--- pages/api/produit/[id].js
+++ pages/api/produit/[id].js
@@ -1,5 +1,5 @@
 // pages/api/produit/[id].js
-import { getProduitById } from '../../../lib/supabase';
+const { getProduitById } = require('../../../lib/supabase');
 
 export default async function handler(req, res) {
   const { id } = req.query;
